42d5e48dc523f61b3f163f7248bd13c9e452f40f
Anton Saweliew <ansaweliew109@gmail.com>
2025-07-27 14:47:58 +0300

    добавил видео на сайт

--- src/app/components/for-employers/for-employers.component.ts
+++ src/app/components/for-employers/for-employers.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-for-employers',
   templateUrl: './for-employers.component.html',
   styleUrls: ['./for-employers.component.scss']
 })
-export class ForEmployersComponent {
+export class ForEmployersComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
+  isPlaying = false;
+  progress = 0;
+  private progressInterval: any;
+
+  ngAfterViewInit() {
+    const video = this.videoPlayer.nativeElement;
+    
+    video.addEventListener('play', () => {
+      this.isPlaying = true;
+      this.startProgressTracking();
+    });
+
+    video.addEventListener('pause', () => {
+      this.isPlaying = false;
+      this.stopProgressTracking();
+    });
+
+    video.addEventListener('ended', () => {
+      this.isPlaying = false;
+      this.progress = 0;
+      this.stopProgressTracking();
+    });
+
+    video.addEventListener('timeupdate', () => {
+      if (video.duration) {
+        this.progress = (video.currentTime / video.duration) * 100;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.stopProgressTracking();
+  }
+
+  togglePlay() {
+    const video = this.videoPlayer.nativeElement;
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
+  seekVideo(event: MouseEvent) {
+    const video = this.videoPlayer.nativeElement;
+    const progressBar = event.currentTarget as HTMLElement;
+    const rect = progressBar.getBoundingClientRect();
+    const clickX = event.clientX - rect.left;
+    const percentage = (clickX / rect.width) * 100;
+    
+    if (video.duration) {
+      video.currentTime = (percentage / 100) * video.duration;
+    }
+  }
+
+  private startProgressTracking() {
+    this.progressInterval = setInterval(() => {
+      const video = this.videoPlayer.nativeElement;
+      if (video.duration && !video.paused) {
+        this.progress = (video.currentTime / video.duration) * 100;
+      }
+    }, 100);
+  }
+
+  private stopProgressTracking() {
+    if (this.progressInterval) {
+      clearInterval(this.progressInterval);
+      this.progressInterval = null;
+    }
+  }
 }
--- src/app/components/for-finders/for-finders.component.ts
+++ src/app/components/for-finders/for-finders.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-for-finders',
   templateUrl: './for-finders.component.html',
   styleUrls: ['./for-finders.component.scss']
 })
-export class ForFindersComponent {
+export class ForFindersComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
+  isPlaying = false;
+  progress = 0;
+  private progressInterval: any;
+
+  ngAfterViewInit() {
+    const video = this.videoPlayer.nativeElement;
+    
+    video.addEventListener('play', () => {
+      this.isPlaying = true;
+      this.startProgressTracking();
+    });
+
+    video.addEventListener('pause', () => {
+      this.isPlaying = false;
+      this.stopProgressTracking();
+    });
+
+    video.addEventListener('ended', () => {
+      this.isPlaying = false;
+      this.progress = 0;
+      this.stopProgressTracking();
+    });
+
+    video.addEventListener('timeupdate', () => {
+      if (video.duration) {
+        this.progress = (video.currentTime / video.duration) * 100;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.stopProgressTracking();
+  }
+
+  togglePlay() {
+    const video = this.videoPlayer.nativeElement;
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
+  seekVideo(event: MouseEvent) {
+    const video = this.videoPlayer.nativeElement;
+    const progressBar = event.currentTarget as HTMLElement;
+    const rect = progressBar.getBoundingClientRect();
+    const clickX = event.clientX - rect.left;
+    const percentage = (clickX / rect.width) * 100;
+    
+    if (video.duration) {
+      video.currentTime = (percentage / 100) * video.duration;
+    }
+  }
+
+  private startProgressTracking() {
+    this.progressInterval = setInterval(() => {
+      const video = this.videoPlayer.nativeElement;
+      if (video.duration && !video.paused) {
+        this.progress = (video.currentTime / video.duration) * 100;
+      }
+    }, 100);
+  }
+
+  private stopProgressTracking() {
+    if (this.progressInterval) {
+      clearInterval(this.progressInterval);
+      this.progressInterval = null;
+    }
+  }
 }
